2aa1b7278f405d00be0c2d5a604568f0f3157a4c
stasbychkar <stanislav.bychkar@MacBook-Air-Stanislav-5.local>
2025-09-22 15:53:51 -0700

    Security with Arcjet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 import express from 'express';
+import logger from "#config/logger.js";
+import authRoutes from '#routes/auth.routes.js'
+import helmet from "helmet";
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+import securityMiddleware from '#middleware/security.middleware.js'
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+
+app.use(securityMiddleware)
+
 app.get('/', (req, res) => {
+    logger.info('Hello from Acquisitions!');
     res.status(200).send('Hello from Acquisitions!');
 });
 
-export default app;
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+})
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Acquisitions API is running!' })
+})
+
+app.use('/api/auth', authRoutes);
+
+export default app; 
